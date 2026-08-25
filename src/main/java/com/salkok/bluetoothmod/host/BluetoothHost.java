1377bac7cf02b397ad42236bb5127c73fa6578f7
turba <salkok872@gmail.com>
2026-08-25 16:56:14 +0300

    Update BluetoothHost.java

--- src/main/java/com/salkok/bluetoothmod/host/BluetoothHost.java
+++ src/main/java/com/salkok/bluetoothmod/host/BluetoothHost.java
@@ -3,21 +3,46 @@ package com.salkok.bluetoothmod.host;
 import com.salkok.bluetoothmod.android.AndroidBluetoothBridge;
 
 public class BluetoothHost {
-    private static BluetoothHost instance;
+
+    private static final BluetoothHost INSTANCE =
+            new BluetoothHost();
+
     private final AndroidBluetoothBridge bridge;
 
+    private volatile boolean enabled;
+
     private BluetoothHost() {
-        this.bridge = new AndroidBluetoothBridge();
+        this.bridge =
+                new AndroidBluetoothBridge();
     }
 
     public static BluetoothHost getInstance() {
-        if (instance == null) {
-            instance = new BluetoothHost();
-        }
-        return instance;
+        return INSTANCE;
     }
 
-    public void startHostServer() {
-        bridge.startServer();
+    public void setEnabled(boolean enabled) {
+
+        this.enabled = enabled;
+
+        if (!enabled) {
+            bridge.stopServer();
+        }
     }
-}
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void startHostServer(int minecraftPort) {
+
+        if (!enabled) {
+            return;
+        }
+
+        bridge.startServer(minecraftPort);
+    }
+
+    public void stopHostServer() {
+        bridge.stopServer();
+    }
+                }
