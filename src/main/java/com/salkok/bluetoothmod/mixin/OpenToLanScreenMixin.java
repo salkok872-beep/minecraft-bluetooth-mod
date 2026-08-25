2622a5c51d9fc75a8c3dedf317f9bfb5da8b6129
turba <salkok872@gmail.com>
2026-08-25 16:57:46 +0300

    Update OpenToLanScreenMixin.java

--- src/main/java/com/salkok/bluetoothmod/mixin/OpenToLanScreenMixin.java
+++ src/main/java/com/salkok/bluetoothmod/mixin/OpenToLanScreenMixin.java
@@ -1,10 +1,12 @@
 package com.salkok.bluetoothmod.mixin;
 
 import com.salkok.bluetoothmod.host.BluetoothHost;
+
 import net.minecraft.client.gui.screen.OpenToLanScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,24 +14,57 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(OpenToLanScreen.class)
 public class OpenToLanScreenMixin extends Screen {
-    private boolean bluetoothEnabled = false;
-    private ButtonWidget btnBluetooth;
+
+    private boolean bluetoothEnabled;
+
+    private ButtonWidget bluetoothButton;
 
     protected OpenToLanScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void addBluetoothButton(CallbackInfo ci) {
-        this.btnBluetooth = this.addDrawableChild(ButtonWidget.builder(
-            Text.literal("Bluetooth'a Ac: KAPALI"), 
-            button -> {
-                this.bluetoothEnabled = !this.bluetoothEnabled;
-                button.setMessage(Text.literal("Bluetooth'a Ac: " + (this.bluetoothEnabled ? "ACIK" : "KAPALI")));
-                if (this.bluetoothEnabled) {
-                    BluetoothHost.getInstance().startHostServer();
-                }
-            }
-        ).dimensions(this.width / 2 - 155, this.height / 4 + 120, 150, 20).build());
+    @Inject(
+            method = "init",
+            at = @At("TAIL")
+    )
+    private void addBluetoothButton(
+            CallbackInfo ci
+    ) {
+
+        bluetoothButton =
+                this.addDrawableChild(
+                        ButtonWidget.builder(
+                                Text.literal(
+                                        "Bluetooth'a Aç: KAPALI"
+                                ),
+                                button -> {
+
+                                    bluetoothEnabled =
+                                            !bluetoothEnabled;
+
+                                    button.setMessage(
+                                            Text.literal(
+                                                    "Bluetooth'a Aç: "
+                                                            +
+                                                    (
+                                                            bluetoothEnabled
+                                                                    ? "AÇIK"
+                                                                    : "KAPALI"
+                                                    )
+                                            )
+                                    );
+
+                                    BluetoothHost.getInstance()
+                                            .setEnabled(
+                                                    bluetoothEnabled
+                                            );
+                                }
+                        ).dimensions(
+                                this.width / 2 - 155,
+                                this.height / 4 + 120,
+                                150,
+                                20
+                        ).build()
+                );
     }
-}
+        }
