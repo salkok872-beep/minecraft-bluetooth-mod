60eada12fc5ad0656fffd6a3f3cb087ef269ee51
turba <salkok872@gmail.com>
2026-08-25 16:53:34 +0300

    Update BluetoothBridge.java

--- src/main/java/com/salkok/bluetoothmod/bluetooth/BluetoothBridge.java
+++ src/main/java/com/salkok/bluetoothmod/bluetooth/BluetoothBridge.java
@@ -3,7 +3,12 @@ package com.salkok.bluetoothmod.bluetooth;
 import java.util.List;
 
 public abstract class BluetoothBridge {
-    public abstract void startServer();
+
+    public abstract void startServer(int minecraftPort);
+
     public abstract BluetoothConnection connectToDevice(String address);
+
     public abstract List<BluetoothDeviceInfo> getPairedDevices();
+
+    public abstract void stopServer();
 }
