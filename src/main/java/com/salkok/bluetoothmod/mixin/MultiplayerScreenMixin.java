3e58754fb291986f40d4fd7cc0106170bdb35bf1
turba <salkok872@gmail.com>
2026-08-25 16:59:18 +0300

    Update MultiplayerScreenMixin.java

--- src/main/java/com/salkok/bluetoothmod/mixin/MultiplayerScreenMixin.java
+++ src/main/java/com/salkok/bluetoothmod/mixin/MultiplayerScreenMixin.java
@@ -1,27 +1,52 @@
 package com.salkok.bluetoothmod.mixin;
 
 import com.salkok.bluetoothmod.client.BluetoothClient;
+
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MultiplayerScreen.class)
-public class MultiplayerScreenMixin extends Screen {
-    protected MultiplayerScreenMixin(Text title) {
+public class MultiplayerScreenMixin
+        extends Screen {
+
+    protected MultiplayerScreenMixin(
+            Text title
+    ) {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void addBluetoothJoinButton(CallbackInfo ci) {
-        // Ekranın sağ üst köşesine yerleştirildi (Sağdan 130px, yukarıdan 10px)
-        this.addDrawableChild(ButtonWidget.builder(
-            Text.literal("Bluetooth ile Katıl"), 
-            button -> BluetoothClient.openDeviceSelectionScreen(this.client, this)
-        ).dimensions(this.width - 130, 10, 120, 20).build());
+    @Inject(
+            method = "init",
+            at = @At("TAIL")
+    )
+    private void addBluetoothJoinButton(
+            CallbackInfo ci
+    ) {
+
+        this.addDrawableChild(
+                ButtonWidget.builder(
+                        Text.literal(
+                                "Bluetooth Dünyaları"
+                        ),
+                        button ->
+                                BluetoothClient
+                                        .openDeviceSelectionScreen(
+                                                this.client,
+                                                this
+                                        )
+                ).dimensions(
+                        this.width / 2 - 100,
+                        this.height - 52,
+                        200,
+                        20
+                ).build()
+        );
     }
-}
+    }
