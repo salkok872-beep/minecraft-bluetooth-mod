06dbbea7c6665dd2a3ad65cf3ad68c08ff8f4f06
turba <salkok872@gmail.com>
2026-08-25 09:45:40 +0300

    Update BluetoothConnection.java

--- src/main/java/com/salkok/bluetoothmod/bluetooth/BluetoothConnection.java
+++ src/main/java/com/salkok/bluetoothmod/bluetooth/BluetoothConnection.java
@@ -2,10 +2,11 @@ package com.salkok.bluetoothmod.bluetooth;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.IOException;
 
 public interface BluetoothConnection {
     boolean isConnected();
-    InputStream getInputStream();
-    OutputStream getOutputStream();
+    InputStream getInputStream() throws IOException;
+    OutputStream getOutputStream() throws IOException;
     void close();
 }
