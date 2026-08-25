5afc34099b6ccb0d1695d921edcaf325be168c68
turba <salkok872@gmail.com>
2026-08-25 09:23:27 +0300

    Update BluetoothClient.java

--- src/main/java/com/salkok/bluetoothmod/client/BluetoothClient.java
+++ src/main/java/com/salkok/bluetoothmod/client/BluetoothClient.java
@@ -1,16 +1,12 @@
 package com.salkok.bluetoothmod.client;
 
-import com.salkok.bluetoothmod.BluetoothMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 
 public class BluetoothClient {
-
     public static void openDeviceSelectionScreen(MinecraftClient client, Screen parent) {
-        BluetoothMod.LOGGER.info("Cihaz secim ekrani aciliyor.");
-    }
-
-    public static void connect(String deviceAddress) {
-        BluetoothMod.LOGGER.info("Bluetooth baglantisi kuruluyor: " + deviceAddress);
+        if (client != null) {
+            client.execute(() -> client.setScreen(new BluetoothDeviceSelectionScreen(parent)));
+        }
     }
 }
